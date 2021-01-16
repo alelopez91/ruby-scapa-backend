e0273943e4b5579dee48b13f873f7ef4f8286ea6
Ale Lopez <alelopez.utnfrt@gmail.com>
2021-01-16 03:29:04 -0300

    Added blank empty line
    [pictograms-admin-endpoints]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   has_many :pictograms, dependent: :destroy
+
   validates :description, presence: true
 
   scope :ordered_by_description, -> { order(description: :asc) }
