65f2d6f0f70015a2bff49823c7afd911976c76a4
Ale Lopez <alelopez.utnfrt@gmail.com>
2021-02-06 12:47:32 -0300

    Added destroy dependency to pictograms associated to custom and default categories
    [custom-pic-dependent-destroy]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   validates :description, presence: true
 
-  has_many :pictograms, dependent: :destroy
+  has_many :pictograms, as: :classifiable, dependent: :destroy
 
   scope :ordered_by_description, -> { order(description: :asc) }
 end
--- app/models/custom_category.rb
+++ app/models/custom_category.rb
@@ -3,7 +3,7 @@ class CustomCategory < ApplicationRecord
 
   belongs_to :folder
 
-  has_many :custom_pictograms
+  has_many :pictograms, as: :classifiable, dependent: :destroy
 
   scope :ordered_by_description, -> { order(description: :asc) }
 end
