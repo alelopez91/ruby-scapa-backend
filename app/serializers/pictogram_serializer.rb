98c5f3e8909b22008556af2e03b6069a388ee9f9
Ale Lopez <alelopez.utnfrt@gmail.com>
2021-02-19 14:01:40 -0300

    Added image_url to pictogram_serializer
    [add-image-to-pics]

--- app/serializers/pictogram_serializer.rb
+++ app/serializers/pictogram_serializer.rb
@@ -1,6 +1,10 @@
 class PictogramSerializer
   include JSONAPI::Serializer
-  attributes :description, :image
+
+  attributes :description, :is_custom
+  attribute :image_url, if: Proc.new{ |object| object.image.attached? } do |object|
+    Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true)
+  end
 
   belongs_to :classifiable, serializer: Proc.new { |record|
     "#{record.class}Serializer".constantize
