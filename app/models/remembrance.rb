f33a68fe7fec2e6d8333b5720f7836761a73beda
Ale Lopez <alelopez.utnfrt@gmail.com>
2021-03-26 07:39:00 -0300

    Added dependent: :destroy option to remembrance and routine association tables
    [fix-routines-remembrances-destroy]

--- app/models/remembrance.rb
+++ app/models/remembrance.rb
@@ -1,7 +1,7 @@
 class Remembrance < ApplicationRecord
   belongs_to :folder
 
-  has_many :remembrance_pictograms
+  has_many :remembrance_pictograms, dependent: :destroy
   has_many :pictograms, through: :remembrance_pictograms
 
   scope :ordered_by_description, -> { order(description: :asc) }
--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,5 +1,5 @@
 class Routine < ApplicationRecord
-  has_many :pictogram_routines
+  has_many :pictogram_routines, dependent: :destroy
   has_many :pictograms, through: :pictogram_routines
 
   scope :ordered_by_description, -> { order(description: :asc) }
